a0f66e9e32a415ac5bb6fd5c9b06219432b2e12b
Edward <edpark13@gmail.com>
2015-03-23 11:21:23 -0700

    create profile model and set urls

--- gamechat/profiles/models.py
+++ gamechat/profiles/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.db.models import Q
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, related_name='profile')
+    friends = models.ManyToManyField('Profile', symmetrical=True,
+                                       related_name='_friends', null=True,
+                                       blank=True)
+    blocking = models.ManyToManyField('Profile', symmetrical=False,
+                                      related_name='_blocking', null=True,
+                                      blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+    def friending(self, other_profile):
+        return self.friends.add(other_profile)
+
+    def unfriending(self, other_profile):
+        return self.friends.remove(other_profile)
+
+    def get_friends(self):
+        return Profile.objects.filter(Q(_friends=self) & ~Q(blocking=self) & ~Q(_blocking=self))
+
+    def block(self, other_profile):
+        return self.blocking.add(other_profile)
+
+    def unblock(self, other_profile):
+        return self.block.remove(other_profile)
