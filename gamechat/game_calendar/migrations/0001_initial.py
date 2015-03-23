ac1cad85c00fe958c72359d1622033b2e2e726c2
Robert Haskell <robertwhaskell@gmail.com>
2015-03-23 12:00:30 -0700

    set up html skeleton for game calendar

---
+++ gamechat/game_calendar/migrations/0001_initial.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Article',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=200)),
+                ('pub_date', models.DateField()),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
--- gamechat/game_calendar/views.py
+++ gamechat/game_calendar/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+
+
 # Create your views here.
+
+def calendar(request):
+    return render(request, 'game_calendar/calendar.html', {'calendar': "hello!"})
