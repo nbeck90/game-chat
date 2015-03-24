c84f49b0b4c6b8250fdb64d50c0430d2d3efab2c
Nick Becker <nbeck90@uw.edu>
2015-03-24 14:49:25 -0700

    Add home page

--- gamechat/gamechat/urls.py
+++ gamechat/gamechat/urls.py
@@ -1,14 +1,11 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic import RedirectView
 from django.contrib import admin
 admin.autodiscover()
 
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'gamechat.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^(/)?$', RedirectView.as_view(url='/chat/')),
+
+    url(r'^$', 'gamechat.views.home_page', name='home'),
     url(r'^chat/', include('chat.urls')),
     url(r'^', include('profiles.urls'), name='profile'),
     url(r'^admin/', include(admin.site.urls)),
