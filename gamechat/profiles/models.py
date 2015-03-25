63b9a4fc32fe51421798f32467e0b7fe98924ca0
Robert Haskell <robertwhaskell@gmail.com>
2015-03-25 10:17:44 -0700

    added block/unblock functionality

--- gamechat/profiles/models.py
+++ gamechat/profiles/models.py
@@ -8,17 +8,25 @@ from django.db.models import permalink
 class Profile(models.Model):
     user = models.OneToOneField(User, related_name='profile')
     friends = models.ManyToManyField('Profile', symmetrical=True,
-                                       related_name='friends', null=True,
-                                       blank=True)
+                                     related_name='friends', null=True,
+                                     blank=True)
+
     blocking = models.ManyToManyField('Profile', symmetrical=False,
-                                      related_name='_blocking', null=True,
+                                      related_name='blocked_by', null=True,
                                       blank=True)
+
+    requested_friends = models.ManyToManyField('Profile',
+                                               symmetrical=False,
+                                               related_name='requesting_friend',
+                                               null=True, blank=True)
+
     picture = models.ImageField(
         upload_to='photos/', null=True, blank=True, default='photos/link.jpg')
     slug = models.CharField(max_length=32, unique=True, blank=True)
 
     Created_room = False
-    chat_room_name = models.CharField(max_length=64, unique=True, blank=True, null=True)
+    chat_room_name = models.CharField(max_length=64, unique=True,
+                                      blank=True, null=True)
 
     def __str__(self):
         return self.user.username
@@ -38,7 +46,7 @@ class Profile(models.Model):
         return self.friends.remove(other_profile)
 
     def get_friends(self):
-        return Profile.objects.filter(Q(friends=self) & ~Q(blocking=self) & ~Q(_blocking=self))
+        return Profile.objects.filter(Q(friends=self) & ~Q(blocking=self) & ~Q(blocked_by=self))
 
     def block(self, other_profile):
         return self.blocking.add(other_profile)
