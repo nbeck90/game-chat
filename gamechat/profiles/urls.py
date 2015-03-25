bd015d128b0c6b55994f15c0a10321b025594b19
Robert Haskell <roberthaskell1@Roberts-iMac.local>
2015-03-24 22:31:06 -0700

    added a button for the profile list

--- gamechat/profiles/urls.py
+++ gamechat/profiles/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns('profiles.views',
     url(r'^$', 'profile', name='profile'),
     url(r'^(?P<slug>[\w\-]+)/$', 'other_profile', name='other_profile'),
     url(r'^edit$', 'ProfileEdit', name='profile_edit'),
-    url(r'^profile_list$', (views.ListProfiles.as_view(
+    url(r'^profile_list', (views.ListProfiles.as_view(
         template_name="profiles/profile_list.html")),
         name='profile_list'),
     url(r'^friend/(?P<pk>\d+)$', 'make_friends', name='profile_edit'),
