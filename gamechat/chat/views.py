7fd727eb38395f759c5a3975dbc67c22121ba065
efrainc <efrainc88@gmail.com>
2015-03-25 17:33:08 -0700

    Please work

--- gamechat/chat/views.py
+++ gamechat/chat/views.py
@@ -16,6 +16,7 @@ list_of_queus = ['ssb', 'wow', 'lol', 'cs', 'destiny',
                  'mine', 'hearth', 'dota', 'diablo',
                  'local']
 
+
 def check_queues():
     chatrooms = ChatRoom.objects.all()
     for chatroom in chatrooms:
@@ -58,18 +59,19 @@ def create_room(request):
 def chat_room(request, chat_room_id):
     room = ChatRoom.objects.get(pk=chat_room_id)
     room_name = str(room.name)
+    user = request.user.username
     context = {
         'chatroom': room,
         'subs': room.subscribers.all(),
         'rooms': room.name,
         'queues': QUEUES,
     }
+    # print "test"
+    # print request.user.profile
     if request.user.profile:
         room.add_subscriber(request.user.profile)
         check_queues()
-        print request.user.username
-        print QUEUES
-        QUEUES[room_name][request.user.username] = queue.Queue()
+        QUEUES[room_name].update({user: queue.Queue()})
 
     return render(request, 'chat/chat_room.html', context)
 
@@ -79,9 +81,10 @@ def chat_add(request, chat_room_id):
     message = request.POST.get('message')
     chat_room = ChatRoom.objects.get(pk=chat_room_id)
     chat_room_name = chat_room.name
+    user = request.user.username
     for prof in QUEUES[chat_room_name]:
-        msg = "{}:    {}".format(request.user.username, message)
-        QUEUES.get(chat_room_name).get(request.user.username).put_nowait(msg)
+        msg = "{}:    {}".format(user, message)
+        QUEUES.get(chat_room_name).get(user).put_nowait(msg)
 
     return JsonResponse({'message': message})
 
@@ -90,9 +93,13 @@ def chat_add(request, chat_room_id):
 def chat_messages(request, chat_room_id):
     chat_room = ChatRoom.objects.get(pk=chat_room_id)
     chat_room_name = chat_room.name
+    user = request.user.username
+    # print request.user.username
+    # print QUEUES[chat_room_name][user]
+    # check_queues()
+    # print QUEUES
     try:
-        a = QUEUES[chat_room_name][request.user.username]
-        msg = a.get(timeout=5)
+        msg = QUEUES[chat_room_name][user].get(timeout=10)
     except queue.Empty:
         msg = []
 
