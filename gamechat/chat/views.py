e86512e8f972dc9f28b384846c692b9e161df6c8
Nick Becker <nbeck90@uw.edu>
2015-03-26 13:47:43 -0700

    maybe if we put the singleton in the app config it will actually be a singleton.  Fucking django

--- gamechat/chat/views.py
+++ gamechat/chat/views.py
@@ -1,35 +1,16 @@
+from django.apps import apps
 from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import render, redirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse
 from chat.models import ChatRoom
 from gevent import queue
 
 
-class QueueContainer(object):
-    def __new__(cls, *args, **kwargs):
-        if not hasattr(cls, '_instance'):
-            self = super(QueueContainer, cls).__new__(cls, *args, **kwargs)
-            QueueContainer.__init__(self, *args, **kwargs)
-            cls._instance = self
-        return cls._instance
+chat_app = apps.get_app_config('chat')
 
-    def __repr__(self):
-        return "{}: {}".format(id(self._container), str(self._container))
 
-    def __init__(self, *args, **kwargs):
-        self._container = {'Test Chat Room 1': {'generic': queue.Queue(), }, }
+QUEUES = chat_app.QUEUES
 
-    def __setitem__(self, key, value):
-        self._container[key] = value
-
-    def __getitem__(self, key):
-        return self._container[key]
-
-    def __delitem__(self, key):
-        del self._container[key]
-
-QUEUES = QueueContainer()
 
 list_of_queus = ['ssb', 'wow', 'lol', 'cs', 'destiny',
                  'mine', 'hearth', 'dota', 'diablo',
