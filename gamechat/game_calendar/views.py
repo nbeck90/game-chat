cd18c20fd6269761dc26917d48bb3c4273d6116c
Robert Haskell <roberthaskell1@Roberts-iMac.local>
2015-03-26 01:54:08 -0700

    and now you can invite friends to your events

--- gamechat/game_calendar/views.py
+++ gamechat/game_calendar/views.py
@@ -3,6 +3,7 @@ from game_calendar.forms import EventForm
 from profiles.models import Profile
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
+from django.db.models import Q
 
 
 class EventCreate(CreateView):
@@ -25,8 +26,9 @@ def return_event(request):
     from django.http import HttpResponse
     import json
 
+    profile = request.user.profile
     json_list = []
-    for event in Event.objects.all():
+    for event in Event.objects.filter(Q(attending=profile) | Q(creator=profile)):
         json_entry = {
             'title': event.title,
             'start': event.date.strftime("%Y-%m-%dT%H:%M:%S"),
