09534820043309c68dbbbf2431de5f04c764987f
Nick Becker <nbeck90@uw.edu>
2015-03-25 18:08:16 -0700

    update merged versions

--- gamechat/chat/urls.py
+++ gamechat/chat/urls.py
@@ -4,7 +4,7 @@ from chat import views
 
 urlpatterns = patterns('',
                url(r'^$', views.index, name='index'),
-               url(r'^(?P<chat_room_id>\d+)/$', views.chat_room,
+               url(r'^room/(?P<chat_room_id>\d+)/$', views.chat_room,
                    name='chat_room'),
                url(r'^add/(?P<chat_room_id>\d+)/$', views.chat_add,
                    name='chat_add'),
